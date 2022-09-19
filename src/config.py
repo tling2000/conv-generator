6c12a40bd47dfcdc8167332fdd5db8cd9dcacba6
tangling2000 <elftat@icloud.com>
2022-09-20 00:04:49 +0800

    new model

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import os
-from pickle import TRUE
 
 DATE = str(datetime.now().month).zfill(2)+str(datetime.now().day).zfill(2)  
 MOMENT = str(datetime.now().hour).zfill(2)+str(datetime.now().minute).zfill(2) + \
@@ -12,7 +11,7 @@ IMAGE_SHAPE = (224,224)
 IN_CHANNELS = 3
 MID_CHANNELS = 16
 
-CONV_NUM = 50
+CONV_NUM = 51
 
 PARAM_MEAN = 0
 PARAM_STD = 0.1
